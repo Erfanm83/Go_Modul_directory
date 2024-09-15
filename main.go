323abb145fca914d4d1365ce80498700f80573e8
Erfanm83 <erfanmahmudi1383@gmail.com>
2024-09-15 22:12:59 +0330

    Update on files

---
+++ main.go
@@ -0,0 +1,175 @@
+package main
+
+import (
+	// "bufio"
+	"fmt"
+	// "os"
+	// "sort"
+	// "strconv"
+	// "strings"
+)
+
+func main() {
+	// var n int
+	// fmt.Scanf("%d", &n)
+	// m := make(map[string]string)
+
+	// reader := bufio.NewReader(os.Stdin)
+
+	// for i := 0; i < n; i++ {
+	// 	// Read the entire line
+	// 	input, _ := reader.ReadString('\n')
+	// 	input = strings.TrimSpace(input)
+
+	// 	// Split into parts: operation, isbn, and title
+	// 	parts := strings.SplitN(input, " ", 3)
+	// 	q := parts[0]    // Operation (ADD or REMOVE)
+	// 	isbn := parts[1] // ISBN
+	// 	var title string
+	// 	if q == "ADD" {
+	// 		title = parts[2] // Title as a single string
+	// 	}
+	// 	// 		fmt.Printf("title: %v\n", title)
+
+	// 	if q == "ADD" {
+	// 		m[isbn] = title
+	// 	} else if q == "REMOVE" {
+	// 		delete(m, isbn)
+	// 	} else {
+	// 		fmt.Println("Invalid operation:", q)
+	// 		return
+	// 	}
+	// }
+
+	// // Create a slice of keys (ISBNs)
+	// isbns := make([]string, 0, len(m))
+	// for isbn := range m {
+	// 	isbns = append(isbns, isbn)
+	// }
+
+	// // Sort by title, then by ISBN as a number if titles are equal
+	// sort.Slice(isbns, func(i, j int) bool {
+	// 	// Sort by title first
+	// 	if m[isbns[i]] != m[isbns[j]] {
+	// 		return m[isbns[i]] < m[isbns[j]]
+	// 	}
+
+	// 	// If titles are the same, sort by ISBN as a number
+	// 	numI, _ := strconv.Atoi(isbns[i])
+	// 	numJ, _ := strconv.Atoi(isbns[j])
+	// 	return numI < numJ
+	// })
+
+	// // Print the sorted ISBNs
+	// for _, isbn := range isbns {
+	// 	fmt.Println(isbn)
+	// }
+
+	res := introduce("ERfan", "20")
+	fmt.Printf("%s\n", res)
+
+	num1 := 5
+	num2 := 10
+
+	num1, _ = swap(num1, num2)
+
+	fmt.Printf("num1 : %d, num2 : %d\n", num1, num2)
+
+	name, age := getPersonInfo()
+	fmt.Printf("name : %s, age : %d\n", name, age)
+
+	fmt.Printf("%d\n", sum(1, 2, 3, 4, 5, 6, 7, 8, 9))
+
+	//function as a vaiable
+	printName := func(name string) string {
+		return fmt.Sprintf("Your Name : %s\n", name)
+	}
+
+	fmt.Println(printName("Yuri Gagarin"))
+
+	//ananymous function
+	func() {
+		fmt.Println("Anonymous Function")
+	}()
+	func(a, b int) {
+		fmt.Println(a + b)
+	}(4, 5)
+	//ananymous function as a variable
+	subtraction := func(a, b int) {
+		defer fmt.Println("Forth")
+		defer fmt.Println("Third")
+		defer fmt.Println("Second")
+		fmt.Println("First")
+		fmt.Println(a - b)
+	}
+	subtraction(6, 8)
+
+	//higher order function
+	fmt.Println(applyFunction(square, 5))
+	fmt.Println(applyFunction(cube, 5))
+
+	multiply := getMathFunction("multiply")
+	fmt.Println(multiply(5, 25))
+
+}
+
+func applyFunction(f func(int) int, num int) int {
+	return f(num)
+}
+func square(n int) int {
+	return n * n
+}
+func cube(n int) int {
+	return n * n * n
+}
+
+func getMathFunction(operation string) func(int, int) int {
+	switch operation {
+	case "multiply":
+		return func(a, b int) int {
+			return a * b
+		}
+	default:
+		return func(a, b int) int {
+			return a + b
+		}
+	}
+}
+
+func introduce(name string, age string) string {
+	return "My Name is " + name + " and I'm " + age + " years old"
+}
+
+func swap(num1 int, num2 int) (int, int) {
+	return num2, num1
+}
+
+// naked return
+func getPersonInfo() (name string, age int) {
+	name = "Alice"
+	age = 30
+	return
+}
+
+// variadic functions
+func sum(numbers ...int) int {
+	total := 0
+
+	for _, num := range numbers {
+		total += num
+	}
+
+	return total
+}
+
+func IsPalindrome(x int) bool {
+	b := x
+	var r int
+	v := 0
+	for x != 0 {
+		r = x % 10
+		v = v*10 + r
+		x /= 10
+	}
+	return v == b
+}
