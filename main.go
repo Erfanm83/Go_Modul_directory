6c878c4089ab23e8ba9e3a35815bdacc3f7e6c99
Erfanm83 <erfanmahmudi1383@gmail.com>
2024-09-16 23:12:31 +0330

    new learning codes added

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ import (
 )
 
 func main() {
+
+	//////////////////////////////////////////////////// First Strike
 	// var n int
 	// fmt.Scanf("%d", &n)
 	// m := make(map[string]string)
@@ -65,111 +67,154 @@ func main() {
 	// 	fmt.Println(isbn)
 	// }
 
-	res := introduce("ERfan", "20")
-	fmt.Printf("%s\n", res)
+	//////////////////////////////////////////////////// Second Strike
 
-	num1 := 5
-	num2 := 10
+	// res := introduce("ERfan", "20")
+	// fmt.Printf("%s\n", res)
 
-	num1, _ = swap(num1, num2)
+	// num1 := 5
+	// num2 := 10
 
-	fmt.Printf("num1 : %d, num2 : %d\n", num1, num2)
+	// num1, _ = swap(num1, num2)
 
-	name, age := getPersonInfo()
-	fmt.Printf("name : %s, age : %d\n", name, age)
+	// fmt.Printf("num1 : %d, num2 : %d\n", num1, num2)
 
-	fmt.Printf("%d\n", sum(1, 2, 3, 4, 5, 6, 7, 8, 9))
+	// name, age := getPersonInfo()
+	// fmt.Printf("name : %s, age : %d\n", name, age)
 
-	//function as a vaiable
-	printName := func(name string) string {
-		return fmt.Sprintf("Your Name : %s\n", name)
+	// fmt.Printf("%d\n", sum(1, 2, 3, 4, 5, 6, 7, 8, 9))
+
+	// //function as a vaiable
+	// printName := func(name string) string {
+	// 	return fmt.Sprintf("Your Name : %s\n", name)
+	// }
+
+	// fmt.Println(printName("Yuri Gagarin"))
+
+	// //ananymous function
+	// func() {
+	// 	fmt.Println("Anonymous Function")
+	// }()
+	// func(a, b int) {
+	// 	fmt.Println(a + b)
+	// }(4, 5)
+	// //ananymous function as a variable
+	// subtraction := func(a, b int) {
+	// 	defer fmt.Println("Forth")
+	// 	defer fmt.Println("Third")
+	// 	defer fmt.Println("Second")
+	// 	fmt.Println("First")
+	// 	fmt.Println(a - b)
+	// }
+	// subtraction(6, 8)
+
+	// //higher order function
+	// fmt.Println(applyFunction(square, 5))
+	// fmt.Println(applyFunction(cube, 5))
+
+	// multiply := getMathFunction("multiply")
+	// fmt.Println(multiply(5, 25))
+
+	//////////////////////////////////////////////////// Third Strike
+	var a int = 24
+	var p *int = &a
+	fmt.Println(p)  // Referencing
+	fmt.Println(*p) //  Dereferencing
+
+	*p = 27 // Changing the value of where the pointer points at
+	fmt.Println(exampleFunction(p))
+
+	//new variable
+	p_new := new(string)
+	*p_new = "example"
+	fmt.Println(*p_new)
+	fmt.Println(p_new)
+
+	// Call by Value
+	b := 250
+	ChangeVariable(b)
+	fmt.Printf("b : %v\n", b)
+
+	// Call by Reference
+	c := 270
+	ChangeVariableByAddress(&c)
+	fmt.Printf("c : %v\n", c)
+
+	var o *[]int
+	if o == nil {
+		fmt.Println("Gorgali")
 	}
-
-	fmt.Println(printName("Yuri Gagarin"))
-
-	//ananymous function
-	func() {
-		fmt.Println("Anonymous Function")
-	}()
-	func(a, b int) {
-		fmt.Println(a + b)
-	}(4, 5)
-	//ananymous function as a variable
-	subtraction := func(a, b int) {
-		defer fmt.Println("Forth")
-		defer fmt.Println("Third")
-		defer fmt.Println("Second")
-		fmt.Println("First")
-		fmt.Println(a - b)
-	}
-	subtraction(6, 8)
-
-	//higher order function
-	fmt.Println(applyFunction(square, 5))
-	fmt.Println(applyFunction(cube, 5))
-
-	multiply := getMathFunction("multiply")
-	fmt.Println(multiply(5, 25))
-
 }
 
-func applyFunction(f func(int) int, num int) int {
-	return f(num)
-}
-func square(n int) int {
-	return n * n
-}
-func cube(n int) int {
-	return n * n * n
+// func applyFunction(f func(int) int, num int) int {
+// 	return f(num)
+// }
+// func square(n int) int {
+// 	return n * n
+// }
+// func cube(n int) int {
+// 	return n * n * n
+// }
+
+// func getMathFunction(operation string) func(int, int) int {
+// 	switch operation {
+// 	case "multiply":
+// 		return func(a, b int) int {
+// 			return a * b
+// 		}
+// 	default:
+// 		return func(a, b int) int {
+// 			return a + b
+// 		}
+// 	}
+// }
+
+// func introduce(name string, age string) string {
+// 	return "My Name is " + name + " and I'm " + age + " years old"
+// }
+
+// func swap(num1 int, num2 int) (int, int) {
+// 	return num2, num1
+// }
+
+// // naked return
+// func getPersonInfo() (name string, age int) {
+// 	name = "Alice"
+// 	age = 30
+// 	return
+// }
+
+// // variadic functions
+// func sum(numbers ...int) int {
+// 	total := 0
+
+// 	for _, num := range numbers {
+// 		total += num
+// 	}
+
+// 	return total
+// }
+
+// func IsPalindrome(x int) bool {
+// 	b := x
+// 	var r int
+// 	v := 0
+// 	for x != 0 {
+// 		r = x % 10
+// 		v = v*10 + r
+// 		x /= 10
+// 	}
+// 	return v == b
+// }
+
+func exampleFunction(p *int) int {
+	return *p
 }
 
-func getMathFunction(operation string) func(int, int) int {
-	switch operation {
-	case "multiply":
-		return func(a, b int) int {
-			return a * b
-		}
-	default:
-		return func(a, b int) int {
-			return a + b
-		}
-	}
+func ChangeVariable(myVariable int) {
+	myVariable += 1
 }
 
-func introduce(name string, age string) string {
-	return "My Name is " + name + " and I'm " + age + " years old"
-}
-
-func swap(num1 int, num2 int) (int, int) {
-	return num2, num1
-}
-
-// naked return
-func getPersonInfo() (name string, age int) {
-	name = "Alice"
-	age = 30
-	return
-}
-
-// variadic functions
-func sum(numbers ...int) int {
-	total := 0
-
-	for _, num := range numbers {
-		total += num
-	}
-
-	return total
-}
-
-func IsPalindrome(x int) bool {
-	b := x
-	var r int
-	v := 0
-	for x != 0 {
-		r = x % 10
-		v = v*10 + r
-		x /= 10
-	}
-	return v == b
+func ChangeVariableByAddress(address *int) {
+	*address += 1
 }
